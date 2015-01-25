b877802d951986f914826ecf27a9489ad6764f92
Howon Lee <howon@howonlee.com>
2015-01-25 00:03:29 -0800

    fbm is possible, I think

--- rmd.jl
+++ rmd.jl
@@ -10,6 +10,8 @@ function midpoint_bm(maxlevel, sigma=1)
   X[1] = 0
   X[N] = sigma * rand(gauss)
   midpoint_helper(X, 1, N, 1, maxlevel)
+  #there should actually be an analytic expression for the stack depth
+  #in which case we can dispense with the helper
 end
 
 function midpoint_helper(x, idx0, idx2, level, maxlevel, st_devs)
